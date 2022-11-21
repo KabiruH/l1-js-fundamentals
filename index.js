8d12558fac484e942f415d2a87d632188c811e84
KabiruH <kelkab46@gmail.com>
2022-11-21 12:26:26 +0300

    additional files

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ function printValue(value){
 
 // 1.0 DEFINE VARIABLES
 //TODO: ADD fixed variable
+const pi = 3.142;
+printValue(pi)
+var score = 100
+var score = 48
+printValue (score)
 
-
+let age = 17
+printValue(age)
+//let age = 95
 //TODO: ADD redeclarable variable
 
 
@@ -15,31 +22,38 @@ function printValue(value){
 
 // 2.0 DIFFERENT DATA TYPES
 //TODO: Create a fixed variable with a number value
-
+const y = 44
 
 //TODO: Create a redeclarable variable with a boolean value
-
+var isRich = true
 
 //TODO: Create a re-assignable variable with a string value
 
-
+var name = 'kelvinName'
 
 // 3.0 STRING OPERATIONS
 //TODO: Concatenate string (+)
+const message = 'I am in Phase 1.'
+const time = '12.00 PM'
 
+const full = message + time
+printValue (full)
 
 //TODO: Interpolate string (`${}`)
-
-
+const other = `Hello, ${message} and the time is ${time}`
+printValue (other)
 //TODO: Convert to uppercase
-
+const otherVariable = other.toUpperCase()
+printValue (otherVariable)
 
 //TODO: Convert to lowercase
-
+const otherVar = other.toLowerCase()
+printValue (otherVar)
 
 //TODO: Index a specific character
-
-
+var ABCDEFGHIJKLMNOPQRSTUVWXYZ
+printValue('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.charAt(9))
+printValue('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.charAt(0,8,9))  //charAt only takes one parameter
 //TODO: PRACTICE ON YOUR OWN (Check mozilla documentation)
 
 
